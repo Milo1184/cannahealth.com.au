990bc5d3a655e76ae601f1049d1e4ecdfefa025d
Aakash <akash.bhatia1184@gmail.com>
2017-04-19 01:56:33 +1000

    synch

--- wp-content/themes/bridge-child/core.js
+++ wp-content/themes/bridge-child/core.js
@@ -9,16 +9,34 @@ $(document).ready(function() {
 	$('a[href*="#"]:not([href="#"])').click(function() {
 	  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
 		var target = $(this.hash);
+		var subed;
+		  subed = target.length ? true : false; 
 		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 		if (target.length) {
-		  $('html, body').animate({
-			scrollTop: target.offset().top
-		  }, 1000);
+			if(subed) {
+			  $('html, body').animate({
+				scrollTop: target.offset().top
+			  }, 1000);
+			} else {
+			  $('html, body').animate({
+				scrollTop: target.offset().top - 6000
+			  }, 1000);
+			}
 		  return false;
 		}
+	  } else {
+		  var target = $(this).attr("href");
+	$('html,body').animate(
+	   {
+		   scrollTop: $(target).offset().top
+	   },2000,function()
+	   {
+		   location.hash = target;
+	   });		  
 	  }
+		
 	});
-
+	
 	$('.access-forms').click(function() {
 		$('#state-pdfs').slideToggle('fast');
 

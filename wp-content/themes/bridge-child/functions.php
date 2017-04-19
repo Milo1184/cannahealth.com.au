6190a81b858d914717186b1e8b3b8e14b6504be4
Aakash <akash.bhatia1184@gmail.com>
2017-04-19 21:41:46 +1000

    synch

--- wp-content/themes/bridge-child/functions.php
+++ wp-content/themes/bridge-child/functions.php
@@ -4,7 +4,7 @@
 
 Function wp_schools_enqueue_scripts() {
 wp_enqueue_script( 'core', get_stylesheet_directory_uri() . '/core.js', array( 'jquery' ), '1.0.2', true );
-wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css'  );
+wp_register_style( 'childstyle', get_stylesheet_directory_uri() . '/style.css', '1.0.1'  );
 wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'wp_schools_enqueue_scripts', 11);
